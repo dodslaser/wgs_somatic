f225c93b78a475abbe05e1d3bd2fbb5d2e8d62a6
root <root@medair1.medair.lcl>
2020-11-25 16:01:56 +0100

    added input file as argument

--- vcf_conversion.py
+++ vcf_conversion.py
@@ -1,20 +1,7 @@
-#vcf_file = open("/seqstore/webfolders/wgs/barncancer/hg38/DNA68517/DNA68517_200904_AHFKCKDSXY_somatic_refseq3kfilt.vcf.gz", "r")
-
-#print(vcf_file.read())
-
-
-#import vcf
-#vcf_reader = vcf.Reader(open('/home/xshang/vcf_test/DNA66246_200616_BHFNK2DSXY_somatic_refseq3kfilt.vcf', 'r'))
-#vcf_writer = vcf.Writer(open('/home/xshang/vcf_test/DNA66246_200616_BHFNK2DSXY_somatic_refseq3kfilt_modified.vcf', 'w'), vcf_reader)
-
-#for record in vcf_reader:
-#    record = record.FORMAT.replace("AFDP", "DP")
-    #print(record)
-#    vcf_writer.write_record(record)
-
+import sys
 
 #read the input file
-vcf_file = open("/home/xshang/vcf_test/DNA65866_200622_BH77WLDSXY_somatic_refseq3kfilt_Alissa.vcf", "rt")
+vcf_file = open(sys.argv[1], "rt")
 #read file contents to string
 data = vcf_file.read()
 #replace all occurrences of AFDP with DP
@@ -22,7 +9,7 @@ data = data.replace('AFDP', 'DP')
 #close the input file
 vcf_file.close()
 #open the input file in write mode
-vcf_file = open("/home/xshang/vcf_test/DNA65866_200622_BH77WLDSXY_somatic_refseq3kfilt_Alissa.vcf", "wt")
+vcf_file = open(sys.argv[1], "wt")
 #overwrite the input file with the resulting data
 vcf_file.write(data)
 #close the file
