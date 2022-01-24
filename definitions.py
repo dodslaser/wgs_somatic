402255f660faf93c6c92dd2bd3a584016eaa85ee
hannasoderstrom1 <hanna.soderstrom@gu.se>
2022-01-24 11:32:25 +0100

    added logging path

--- definitions.py
+++ definitions.py
@@ -2,3 +2,4 @@ import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 CONFIG_PATH = os.path.join(ROOT_DIR, 'configs', 'wrapper_config.yaml')
+ROOT_LOGGING_PATH = '/medstore/logs/pipeline_logfiles/wgs_somatic'
