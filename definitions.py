8732eeae28326890463644facaaca549526ae741
hannasoderstrom1 <hanna.soderstrom@gu.se>
2022-02-07 09:56:11 +0100

    renamed files

--- definitions.py
+++ definitions.py
@@ -1,5 +1,5 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-CONFIG_PATH = os.path.join(ROOT_DIR, 'configs', 'wrapper_config.yaml')
+CONFIG_PATH = os.path.join(ROOT_DIR, 'configs', 'run-wrapper_config.yaml')
 ROOT_LOGGING_PATH = '/medstore/logs/pipeline_logfiles/wgs_somatic'
