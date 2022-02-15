dc63adfb537cf826b33b07b995562bf805b0755b
hannasoderstrom1 <hanna.soderstrom@gu.se>
2022-02-15 13:08:00 +0100

    get Rctx and Sctx for runs with additional fastqs

--- wgs_somatic-run-wrapper.py
+++ wgs_somatic-run-wrapper.py
@@ -18,6 +18,11 @@ from tools.slims import get_sample_slims_info, SlimsSample, more_fastqs, get_pai
 logger = setup_logger('wrapper', os.path.join(ROOT_LOGGING_PATH, 'WS_wrapper.log'))
 
 
+# Store info about samples to use for sending report emails
+sample_status = {'missing_slims': [],
+                 'unset_WS': [],
+                 'approved': []}
+
 def look_for_runs(root_path):
 # Look for runs in demultiplexdir
     found_paths = glob.glob(os.path.join(root_path, '*'))
@@ -25,11 +30,74 @@ def look_for_runs(root_path):
     return [path for path in found_paths if re.search(regex, os.path.basename(path))]
 
 
+def Rctx_Sctx_for_run(Rctx):
+    '''Create Sctx for a demultiplexed run'''
+    #Sctx_list = []
 
+    # Read demultiplex stats file for sample names, fastq paths, and nr reads
+    with open(Rctx.demultiplex_summary_path, 'r') as inp:
+        demuxer_info = json.load(inp)
+
+    # Store info about samples to use for sending report emails
+#    sample_status = {'missing_slims': [],
+#                     'unset_WS': [],
+#                     'approved': []}
+
+    for sample_id, sample_info in demuxer_info['samples'].items():
+        logger.info(f'Setting up context for {sample_id}.')
+        #print(sample_id, sample_info)
+
+        # Setup Sample context class and add listed fastq paths and nr reads
+        Sctx = SampleContext(sample_id)
+        Sctx.add_fastq(sample_info['fastq_paths'])
+        #print(Sctx.fastqs)
+
+        # Query Slims for clinical information and add to sample context
+        logger.info(f'Fetching SLIMS info.')
+        get_sample_slims_info(Sctx, run_tag = Rctx.run_tag)  # NOTE: Mod with slims info in-place
+
+        if not Sctx.slims_info:
+            logger.warning(f'No SLIMS info available!')
+            logger.warning(f'Sample will not be analysed.')
+            sample_status['missing_slims'].append(Sctx)
+            continue
+        print(f'slims info: {Sctx.slims_info}')
+
+        # NOTE: 54 is Slims internal primary key for wgs_somatic
+        if 54 not in Sctx.slims_info['secondary_analysis']:
+            sample_status['unset_WS'].append(Sctx)
+            continue
+        # Add sample context to run context list
+        logger.info(f'Adding sample context to list of contexts.')
+        Rctx.add_sample_context(Sctx)
+        #print(f'Sctx: {Sctx}')
+        #Sctx_list.append(Sctx)
+
+        if not Rctx.sample_contexts:
+            # If no samples set for wgs_somatic
+            logger.info('No samples set for wgs_somatic. Skipping run.')
+            continue
+
+    for Sctx in Rctx.sample_contexts:
+    #    print(f'Sctx: {Sctx}')
+        sample_status['approved'].append(Sctx)
+    #runID = Rctx.run_name
+    #print(f'sample status: {sample_status}')
+    #tumor_samples = []
+    #normal_samples = []
+
+    return Rctx
 
 def wrapper():
 
+    # Store info about samples to use for sending report emails
+#    sample_status = {'missing_slims': [],
+#                     'unset_WS': [],
+#                     'approved': []}
+
     additional_run_paths = []
+    tumor_samples = []
+    normal_samples = []
 
     with open(CONFIG_PATH, 'r') as conf:
         config = yaml.safe_load(conf)
@@ -37,7 +105,7 @@ def wrapper():
     # Grab all available local run paths
     instrument_root_path = config['novaseq']['demultiplex_path']
     local_run_paths = look_for_runs(instrument_root_path)
-    #print(local_run_paths)
+    #print(f'local_run_paths: {local_run_paths}')
 
     # Read all previously analysed runs
     previous_runs_file = config['previous_runs_file_path']
@@ -69,49 +137,19 @@ def wrapper():
             logger.info(f'Writing {Rctx.run_name} to previous runs list.')
             print(Rctx.run_name, file=prev)
 
-        # Read demultiplex stats file for sample names, fastq paths, and nr reads
-        with open(Rctx.demultiplex_summary_path, 'r') as inp:
-            demuxer_info = json.load(inp)
+        Rctx_run = Rctx_Sctx_for_run(Rctx)
+        #Sctx_list = Rctx_run.sample_contexts
+        #print(f'Sctx_list: {Sctx_list}')
 
-        # Store info about samples to use for sending report emails
-        sample_status = {'missing_slims': [],
-                         'unset_WS': [],
-                         'approved': []}
+       
 
-        for sample_id, sample_info in demuxer_info['samples'].items():
-            logger.info(f'Setting up context for {sample_id}.')
-            #print(sample_id, sample_info)
-
-            # Setup Sample context class and add listed fastq paths and nr reads
-            Sctx = SampleContext(sample_id)
-            Sctx.add_fastq(sample_info['fastq_paths'])
-            #print(Sctx.fastqs)
-
-            # Query Slims for clinical information and add to sample context
-            logger.info(f'Fetching SLIMS info.')
-            get_sample_slims_info(Sctx, run_tag = Rctx.run_tag)  # NOTE: Mod with slims info in-place
-            
-            if not Sctx.slims_info:
-                logger.warning(f'No SLIMS info available!')
-                logger.warning(f'Sample will not be analysed.')
-                sample_status['missing_slims'].append(Sctx)
-                continue
-            print(Sctx.slims_info)
-
-
-
-
-            # NOTE: 54 is Slims internal primary key for wgs_somatic
-            if 54 not in Sctx.slims_info['secondary_analysis']:
-                sample_status['unset_WS'].append(Sctx)
-                continue
-
-
-            # Add sample context to run context list
-            logger.info(f'Adding sample context to list of contexts.')
-            Rctx.add_sample_context(Sctx)
-            #print(Sctx)
-
+#        for Sctx in Rctx_run.sample_contexts:
+#            print(f'Sctx: {Sctx}')
+#            sample_status['approved'].append(Sctx)
+        #runID = Rctx.run_name
+#        print(f'sample_status: {sample_status}')
+        #tumor_samples = []
+        #normal_samples = []
 
             #more_fqs_path = more_fastqs(Sctx, run_tag = Rctx.run_tag)
             #if more_fqs_path:
@@ -119,12 +157,15 @@ def wrapper():
             #    Sctx.add_fastq(more_fqs_path)
             #print(get_pair(Sctx, Rctx.run_tag))
 
-            run_paths = get_pair(Sctx, Rctx.run_tag)
+        #print(f'Sctx_list {Sctx_list}')
+
+        for sctx in Rctx_run.sample_contexts:
+            run_paths = get_pair(sctx, Rctx.run_tag)
             #print(f'run paths: {run_paths}')
             if not run_paths == None:
                 run_paths = list(chain.from_iterable(run_paths))
                 for r in run_paths:
-                    #print(f'r: {r}')
+                    print(f'r: {r}')
                     additional_run_paths.append(r) if r not in additional_run_paths else additional_run_paths
 #            more_fqs_dicts = get_pair(Sctx, Rctx.run_tag)
 #            if more_fqs_dicts:
@@ -141,21 +182,17 @@ def wrapper():
             #print(f'all fastq paths: {Sctx.fastqs}')
                 #Sctx.add_fastq(more_fqs_path)
 
-#        print(Rctx.sample_contexts)
-
-        if not Rctx.sample_contexts:
-            # If no samples set for wgs_somatic
-            logger.info('No samples set for wgs_somatic. Skipping run.')
-            continue
-
-        for Sctx in Rctx.sample_contexts:
-            sample_status['approved'].append(Sctx)
-        runID = Rctx.run_name
-    print(sample_status)
-    tumor_samples = []
-    normal_samples = []
+        #print(f'rctx sample contexts: {Rctx.sample_contexts}')
 
     print(f'additional run paths: {additional_run_paths}')
+    print(f'sample_status: {sample_status}')
+
+    for run_path in additional_run_paths:
+        additionalRctx = RunContext(run_path)
+        additionalRctx = Rctx_Sctx_for_run(additionalRctx)
+        print(f'additional Rctx: {additionalRctx}')
+        
+    print(f'sample_status: {sample_status}')
 
     for category, contexts in sample_status.items():
         print(category) 
