2a6b3a0a737bd20eb722cc33ef7aaa1ebc42c9e7
hannasoderstrom1 <hanna.soderstrom@gu.se>
2022-04-25 08:24:02 +0200

    changed email addresses and added error email

--- tools/email.py
+++ tools/email.py
@@ -13,8 +13,8 @@ def send_email(subject, body):
 
     msg['Subject'] = subject
     msg['From'] = "cgg-cancer@gu.se" # TODO Get from config
-    msg['To'] = "cgg-cancer@gu.se" # TODO Get from config and have different recipients for errors and success
-    msg['Cc'] = "hanna.soderstrom@gu.se" # TODO Get from config
+    msg['To'] = "gms_btb@gu.se" # TODO Get from config and have different recipients for errors and success
+    msg['Cc'] = "cgg-cancer@gu.se" # TODO Get from config
 
 
     # Send the message
@@ -54,8 +54,24 @@ def end_email(run_name, samples):
     send_email(subject, body)
 
 
-#def error_email():
+def error_email(run_name, ok_samples, not_ok_samples):
+    """Send an email about which samples have failed and which samples have succeeded"""
 
+    subject = f'WGS Somatic end mail {run_name}'
 
+    body = '\n'.join([f'WGS somatic has finished successfully for the following samples in run {run_name}:',
+                      '',
+                      f'{new_line}{new_line.join(ok_samples)}',
+                      '',
+                      'The following samples have not finished correctly:',
+                      '',
+                      f'{new_line}{new_line.join(not_ok_samples)}',
+                      '',
+                      'Errors concerning these samples will be investigated.',
+                      '',
+                      'Best regards,',
+                      'CGG Cancer'])
+
+    send_email(subject, body)
 
 
