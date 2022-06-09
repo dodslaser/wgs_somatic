c65b69e9595b7dfb54160ff82e6f8eb6ee067e00
hannasoderstrom1 <hanna.soderstrom@gu.se>
2022-06-09 13:39:32 +0200

    DP+AF for both tumor and normal in FORMAT+sample columns instead of INFO column

--- workflows/scripts/fix_pindelDPoAF.py
+++ workflows/scripts/fix_pindelDPoAF.py
@@ -15,20 +15,43 @@ def main():
     vcf_in = args.vcf_input
     vcf_out = args.vcf_output
 
-    vcf_in = VariantFile(vcf_in)
+    vcf_in = VariantFile(vcf_in, 'r')
     new_header = vcf_in.header
-    new_header.info.add("DP","1","Integer","Sum of AD fields")
-    new_header.info.add("AF","1","Float","Alt AD / sum(AD)")
+
+    # Add DP and AD to FORMAT column
+    new_header.formats.add("DP","1","Integer","Sum of AD fields")
+    new_header.formats.add("AF","1","Float","Alt AD / sum(AD)")
 
     vcf_out = VariantFile(vcf_out, 'w', header=new_header)
 
     for record in vcf_in.fetch():
+        # Calculate DP for tumor
         dp = sum(record.samples[0].get("AD"))
-        record.info["DP"]=dp
-        af=record.samples[0].get("AD")[1]/dp
-        record.info["AF"]=af
-        vcf_out.write(record)
+        # Add value of DP to tumor sample column
+        record.samples[0]["DP"]=dp
+        # Calculate AF for tumor
+        if dp == 0:
+            # Solve division by zero problem if DP = 0
+            af = 0
+        else:
+            af=record.samples[0].get("AD")[1]/dp
+        # Add value of AF to tumor sample column
+        record.samples[0]["AF"]=af
 
+        # Calculate DP for normal
+        n_dp = sum(record.samples[1].get("AD"))
+        # Add value of DP to normal sample column
+        record.samples[1]["DP"]=n_dp
+        # Calculate AF for normal
+        if n_dp == 0:
+            # Solve division by zero problem if DP = 0
+            n_af=0
+        else:
+            n_af=record.samples[1].get("AD")[1]/n_dp
+        # Add value of AF to normal sample column
+        record.samples[1]["AF"]=n_af
+
+        vcf_out.write(record)
 
 if __name__ == "__main__":
     main()
