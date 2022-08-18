f775cc52a962bd3da5a34df841ebe9ea7edb388e
hannasoderstrom1 <hanna.soderstrom@gu.se>
2022-08-18 09:09:20 +0200

    added function for alissa upload

--- launch_snakemake.py
+++ launch_snakemake.py
@@ -59,7 +59,23 @@ def petagene_compress_bam(outputdir, tumorname):
     qsub_script = config["petagene"]["qsub_script"]
     standardout = f"{outputdir}/logs/{tumorname}_petagene_compression_standardout.txt"
     standarderr = f"{outputdir}/logs/{tumorname}_petagene_compression_standarderr.txt"
-    qsub_args = ["qsub", "-N", f"WS-{tumorname}_petagene_compress_bam", "-q", queue, "-o", standardout, "-e", standarderr, qsub_script, outputdir]
+    qsub_args = ["qsub", "-N", f"WGSSomatic-{tumorname}_petagene_compress_bam", "-q", queue, "-o", standardout, "-e", standarderr, qsub_script, outputdir]
+    subprocess.call(qsub_args, shell=False)
+
+def alissa_upload(outputdir, normalname, runnormal, ref=False):
+    ref = 'hg38' # reference genome, change so it can also be hg19. but probably shouldn't upload vcf for hg19 samples
+    size = '240_000_000' # needed as argument for alissa upload. if vcf is larger than this, it is split
+    date, _, _, chip, *_ = runnormal.split('_')
+    normalid= '_'.join([normalname, date, chip])
+    vcfpath = f"{outputdir}/{normalid}_{ref}_SNV_CNV_germline.vcf.gz"
+    config = read_wrapperconf()
+    logger(f"Uploading vcf to Alissa for {normalname}")
+    queue = config["alissa"]["queue"]
+    threads = config["alissa"]["threads"]
+    qsub_script = config["alissa"]["qsub_script"]
+    standardout = f"{outputdir}/logs/{normalid}_alissa_upload_standardout.txt"
+    standarderr = f"{outputdir}/logs/{normalid}_alissa_upload_standarderr.txt"
+    qsub_args = ["qsub", "-N", f"WGSSomatic-{normalname}_alissa_upload", "-q", queue, "-o", standardout, "-e", standarderr, qsub_script, normalname, vcfpath, size, ref]
     subprocess.call(qsub_args, shell=False)
 
 def analysis_main(args, output, runnormal=False, normalname=False, normalfastqs=False, runtumor=False, tumorname=False, tumorfastqs=False, igvuser=False, hg38ref=False, starttype=False):
@@ -302,6 +318,8 @@ if __name__ == '__main__':
             if args.normalsample:
             # these functions are only executed if snakemake workflow has finished successfully
                 yearly_stats(args.tumorsample, args.normalsample)
+                if args.hg38ref:
+                    alissa_upload(args.outputdir, args.normalsample, args.runnormal, args.hg38ref)
                 if not args.nocompress:
                     petagene_compress_bam(args.outputdir, args.tumorsample)    
             else:
@@ -310,5 +328,7 @@ if __name__ == '__main__':
                     petagene_compress_bam(args.outputdir, args.tumorsample)
         else:
             yearly_stats('None', args.normalsample)
+            if args.hg38ref:
+                alissa_upload(args.outputdir, args.normalsample, args.runnormal, args.hg38ref)
             if not args.nocompress:
                 petagene_compress_bam(args.outputdir, args.normalsample)
